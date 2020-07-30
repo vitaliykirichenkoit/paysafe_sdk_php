56e632e87774be6ad4364b3ba5be09cf1dd943cc
Alex <sanyokerem@gmail.com>
2020-07-30 11:58:24 +0300

    RT-10974 : EFTSingleUseToken and ACHSingleUseToken are updated

--- source/Paysafe/CustomerVault/EFTSingleUseToken.php
+++ source/Paysafe/CustomerVault/EFTSingleUseToken.php
@@ -3,6 +3,9 @@
 namespace Paysafe\CustomerVault;
 
 /**
+ * @property string $id
+ * @property string $paymentToken
+ * @property int $timeToLiveSeconds
  * @property Profile $profile
  * @property EFTBankaccounts $eftBankAccount
  * @property BillingAddress $billingAddress
@@ -13,6 +16,9 @@ class EFTSingleUseToken extends \Paysafe\JSONObject
      * @var array
      */
     protected static $fieldTypes = array(
+        'id' => 'string',
+        'paymentToken' => 'string',
+        'timeToLiveSeconds' => 'int',
         'profile' => '\Paysafe\CustomerVault\Profile',
         'eftBankAccount' => '\Paysafe\CustomerVault\EFTBankaccounts',
         'billingAddress' => '\Paysafe\CustomerVault\BillingAddress',
